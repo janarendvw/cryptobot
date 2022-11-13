13bd6a2e383715c17eb6c3fb4cc70f462d9134d1
Jan Arend <janarendvw@gmail.com>
2022-11-13 11:42:02 +0100

    implemented class based system

--- kucoin.py
+++ kucoin.py
@@ -1,82 +1,98 @@
 import requests
 import time, base64, hashlib, hmac
 
+class Main:
+    def auth_request(user, url):
+        BASE_URL = 'https://api.kucoin.com'
+        now = str(int(time.time() * 1000))
+        str_to_sign = str(now) + 'GET' + url
+        signature = base64.b64encode(hmac.new(user.secret.encode('utf-8'), str_to_sign.encode('utf-8'), hashlib.sha256).digest())
+        passphrase = base64.b64encode(hmac.new(user.secret.encode('utf-8'), user.passphrase.encode('utf-8'), hashlib.sha256).digest())
+        headers = {
+            'KC-API-KEY': user.key,
+            'KC-API-SIGN': signature,
+            'KC-API-TIMESTAMP': str(now),
+            'KC-API-PASSPHRASE': passphrase,
+            'KC-API-KEY-VERSION': '2'
+            }
+        r = requests.get(BASE_URL + url, headers=headers).json()
+        return r['data']
+    
+    def request(url):
+        BASE_URL = 'https://api.kucoin.com'
+        r = requests.get(BASE_URL + url).json()
+        return r['data']
 
-def get_user(key: str, api_passphrase: str, api_secret: str, type='main') -> dict:
-    """ Returns the user's account information
+class Api_user:
+    def __init__(self, key, passphrase, secret):
+        self.key = key
+        self.passphrase = passphrase
+        self.secret = secret
+
+
+class Market_data:
+    def get_user(user, type='main') -> dict:
+        """ Returns the user's account information
+            Parameters
+            ----------
+            user: Api_user
+                The user to get the account information of
+            type : str
+                The type of account to get the information from (main, margin or trade). Defaults to main
+        """
+        url = '/api/v1/accounts?type=' + type
+        return Main.auth_request(user, url)
+
+    @staticmethod
+    def get_price_of(ticker: str) -> float:
+        """
+        Returns the current price of a ticker from KuCoin
+
         Parameters
         ----------
-        key : str
-            The API key
-        api_passphrase : str
-            The API passphrase
-        api_secret : str
-            The API secret
-        type : str
-            The type of account to get the information from (main, margin or trade). Defaults to main
-    """
-    BASE_URL = 'https://api.kucoin.com'
-    URL = '/api/v1/accounts?type=' + type
-    now = str(int(time.time() * 1000))
-    str_to_sign = str(now) + 'GET' + '/api/v1/accounts?type=' + type
-    signature = base64.b64encode(hmac.new(api_secret.encode('utf-8'), str_to_sign.encode('utf-8'), hashlib.sha256).digest())
-    passphrase = base64.b64encode(hmac.new(api_secret.encode('utf-8'), api_passphrase.encode('utf-8'), hashlib.sha256).digest())
-    headers = {'KC-API-KEY': key, 'KC-API-SIGN': signature,
-               'KC-API-TIMESTAMP': str(now), 'KC-API-PASSPHRASE': passphrase, 'KC-API-KEY-VERSION': '2'}
-    r = requests.get(BASE_URL + URL, headers=headers).json()
-    return r
+        ticker : str
+            The ticker to get the price of
+        """
+        url = 'https://api.kucoin.com/api/v1/market/orderbook/level1?symbol='
+        return Main.request(url + ticker)['price']
+
+    @staticmethod
+    def get_price_of_list(tickers):
+        """ Returns a list of prices for a list of tickers 
+
+        Parameters
+        ----------
+        tickers : list
+            A list of tickers to get the price of
+        """
+        prices = {}
+        for ticker in tickers:
+            prices[ticker] = Market_data.get_price_of(ticker)
+        return prices
 
 
-def get_price_of(ticker: str) -> float:
-    """
-    Returns the current price of a ticker from KuCoin
+    def get_24h_stats(ticker: str) -> dict:
+        """ Returns the 24h stats for a ticker 
 
-    Parameters
-    ----------
-    ticker : str
-        The ticker to get the price of
-    """
-    URL = 'https://api.kucoin.com/api/v1/market/orderbook/level1?symbol='
-    r = requests.get(URL + ticker).json()
-    return r['data']['price']
+        Parameters
+        ----------
+        ticker : str
+            The ticker to get the 24h stats of
+        """
+        url = 'https://api.kucoin.com/api/v1/market/stats?symbol='
+        return Main.request(url + ticker)['data']
 
 
-def get_price_of_list(tickers):
-    """ Returns a list of prices for a list of tickers 
+    @staticmethod
+    def get_24h_stats_list(tickers: list) -> dict:
+        """ Returns the 24h stats for a list of tickers 
 
-    Parameters
-    ----------
-    tickers : list
-        A list of tickers to get the price of
-    """
-    prices = {}
-    for ticker in tickers:
-        prices[ticker] = get_price_of(ticker)
-    return prices
-
-
-def get_24h_stats(ticker: str) -> dict:
-    """ Returns the 24h stats for a ticker 
-
-    Parameters
-    ----------
-    ticker : str
-        The ticker to get the 24h stats of
-    """
-    URL = 'https://api.kucoin.com/api/v1/market/stats?symbol='
-    r = requests.get(URL + ticker).json()
-    return r['data']
-
-
-def get_24h_stats_list(tickers: list) -> dict:
-    """ Returns the 24h stats for a list of tickers 
-
-    Parameters
-    ----------
-    tickers : list
-        A list of tickers to get the 24h stats of
-    """
-    stats = {}
-    for ticker in tickers:
-        stats[ticker] = get_24h_stats(ticker)
-    return stats
+        Parameters
+        ----------
+        tickers : list
+            A list of tickers to get the 24h stats of
+        """
+        stats = {}
+        for ticker in tickers:
+            stats[ticker] = Market_data.get_24h_stats(ticker)
+        return stats
